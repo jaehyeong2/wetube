8aae7d86c6b26fc3b9ab6bf19729e94f745908fc
jae <wogud1514>
2021-09-03 15:10:45 +0900

    퍼그 컨디셔널까지

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,11 @@
-export const trending = (req,res) => res.render("home");
-export const see = (req,res)=> res.send("Watch");
-export const edit = (req,res) => res.send("Edit");
+const fakeUser = {
+    username: "Nico",
+    loggedIn: false
+}
+
+export const trending = (req,res) => res.render("home",{pageTitle : "Home", fakeUser});
+export const see = (req,res)=> res.render("watch");
+export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search User");
 export const deleteVideo = (req,res) => res.send("Delete Video");
 export const upload = (req,res) => res.send("Upload video");
