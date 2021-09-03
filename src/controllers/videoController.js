e0810a3c6209d2314791f89f419f3fbe3f3cd994
jae <wogud1514>
2021-09-03 19:59:20 +0900

    watch까지

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,34 @@
-const fakeUser = {
-    username: "Nico",
-    loggedIn: false
+let videos = [{
+    title : "First Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 1,
+},
+{
+    title : "Second Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 1,
+    id : 2,
+},
+{
+    title : "Third Video",
+    rating : 5,
+    comments : 2,
+    createdAt : "2 minutes ago",
+    views : 59,
+    id : 3,
 }
-
-export const trending = (req,res) => res.render("home",{pageTitle : "Home", fakeUser});
-export const see = (req,res)=> res.render("watch");
+]
+export const trending = (req,res) => res.render("home",{pageTitle : "Home", videos});
+export const watch = (req,res)=> {
+    const id = req.params.id;
+    const video =videos[id-1];   
+    res.render("watch",{ pageTitle: `Watching ${video.title}`,video});
+}
 export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search User");
 export const deleteVideo = (req,res) => res.send("Delete Video");
