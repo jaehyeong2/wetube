8a0a7f1638de2df565fbc234dca3c8fab2bce4bb
jae <wogud1514>
2021-09-05 21:35:42 +0900

    email 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,8 +30,8 @@ export const postUpload = async(req,res) => {
    const {title, description, hashtags} = req.body;
    try{
    await Video.create({
-       title : {type: String, required:true},
-       description: {type: String, required:true},
+       title, 
+       description,
        createdAt: Date.now(),
        hashtags:hashtags.split(",").map((word) => `#${word}`),
      
