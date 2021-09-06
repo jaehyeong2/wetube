661c79d3297418ffb6a879effe0282ee2cfdefd5
jae <wogud1514@naver.com>
2021-09-06 13:59:47 +0900

    static이용해서 hashtag 구현

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,16 +29,14 @@ export const getEdit = async(req,res) => {
 export const postEdit = async (req,res) => {
     const id = req.params.id;
     const { title, description, hashtags} = req.body;
-    const video = await Video.exist({_id : id});
+    const video = await Video.exists({ _id: id });
     if(!video){
         return res.render("404", { pageTitle : "Video not found"});
     }
     await Video.findByIdAndUpdate(id,{
         title, 
         description, 
-        hashtags : hashtags
-        .split(",")
-        .map((word) => (word.startsWith(`#`)? word:`#${word}`))
+        hashtags : Video.formatHashtags(hashtags)
     });
     
     return res.redirect(`/videos/${id}`);
@@ -54,9 +52,7 @@ export const postUpload = async (req, res) => {
       await Video.create({
         title,
         description,
-        hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith(`#`)? word:`#${word}`))
+        hashtags: Video.formatHashtags(hashtags)
       });
       return res.redirect("/");
     } catch (error) {
