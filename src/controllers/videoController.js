5bb6530aa612899ed0363fa00fdf72d969544032
jae <wogud1514@naver.com>
2021-09-07 00:02:44 +0900

    로그인 에러메세지까지

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,7 +10,7 @@ export const watch = async(req,res)=> {
     const id = req.params.id; 
     const video = await Video.findById(id);
     if(!video){
-        return res.render("404",{ pageTitle : "page not found"});
+        return res.status(404).render("404",{ pageTitle : "page not found"});
     }else{
         return res.render("watch",{ pageTitle: video.title, video});
     }
@@ -20,7 +20,7 @@ export const getEdit = async(req,res) => {
     const id = req.params.id;
     const video = await Video.findById(id);
     if(!video){
-        return res.render("404", { pageTitle : "Video not found"});
+        return res.status(404).render("404", { pageTitle : "Video not found"});
     } else{
         return res.render("edit",{pageTitle : `Editing: ${video.title} `, video});
     }
@@ -31,7 +31,7 @@ export const postEdit = async (req,res) => {
     const { title, description, hashtags} = req.body;
     const video = await Video.exists({ _id: id });
     if(!video){
-        return res.render("404", { pageTitle : "Video not found"});
+        return res.status(404).render("404", { pageTitle : "Video not found"});
     }
     await Video.findByIdAndUpdate(id,{
         title, 
@@ -56,7 +56,7 @@ export const postUpload = async (req, res) => {
       });
       return res.redirect("/");
     } catch (error) {
-      return res.render("upload", {
+      return res.status(400).render("upload", {
         pageTitle: "Upload Video",
         errorMessage: error._message,
       });
